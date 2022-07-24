0944a5ea803e64cde644f12bc88de485b3460ed9
Erick <erickepc2099@gmail.com>
2022-07-24 15:02:12 -0400

    Enumerando paises

---
+++ Enumerar_Paises.py
@@ -0,0 +1,16 @@
+"""
+Realice un programa que enumere los paises de la siguiente lista.
+paises = ['Canada', 'USA', 'Mexico', 'Australia']
+"""
+
+# Lista de paises
+paises = ['Canada', 'USA', 'Mexico', 'Australia', 'Republica Dominicana']
+# Variable numerica
+num = 0
+# Ciclo for
+for i in paises:
+    # Variable numerica incrementandose
+    num += 1
+    # Se imprime la variable y el ciclo for.
+    print(f'{num}: {i}')
+
