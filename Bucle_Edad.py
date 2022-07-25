9d1176ad879013162895f8552f615748d1965da9
Erick <erickepc2099@gmail.com>
2022-07-24 20:59:20 -0400

    Bucle while Edad

---
+++ Bucle_Edad.py
@@ -0,0 +1,11 @@
+'''
+Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido
+(desde 1 hasta su edad).
+'''
+
+x = int(input('Proporcione su edad: '))
+i = 0
+
+while i != 21:
+    print(f'Has Cumplido: {i}')
+    i += 1
