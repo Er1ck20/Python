4a0ccaa4c69b967538ec1ad00012991090f7d40e
Erick <erickepc2099@gmail.com>
2022-07-27 22:14:43 -0400

    Par e Impar

---
+++ Par_Impar.py
@@ -0,0 +1,11 @@
+# Ejercicio par e impar
+num = int(input('Ingrese un numero: '))
+
+# Preguntamos, si el modulo de dos es igual a 0
+if num % 2 == 0:
+    # El numero sera par.
+    print(f'El numero {num} es par.')
+    
+else:
+    # Si no es igual a 0, el numero es impar.
+    print(f'El numero {num} es impar.')
