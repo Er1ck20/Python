f6fed118d0021beeb93dc01974db79b09c4851c4
Erick <erickepc2099@gmail.com>
2022-07-31 13:59:44 -0400

    Ordenando una lista

---
+++ Ordenando_una_lista.py
@@ -0,0 +1,11 @@
+# Ordenar una lista
+
+lista = [1, 3, 12, 15, 0, 19, 2, -1]
+
+# Ordenar una lista de manera ascendete
+lista.sort()
+print(lista)
+
+# Ordenar una lista de manera descendete
+lista.sort(reverse=True)
+print(lista)
