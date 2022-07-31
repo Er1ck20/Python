cbe0568975674b47767b9c83a14106142b7b58cc
Erick <erickepc2099@gmail.com>
2022-07-31 18:16:59 -0400

    Programa basico para calcular la distancia

---
+++ Calcular_Distancia.py
@@ -0,0 +1,6 @@
+# Calcular Distancia
+
+Velocidad = int(input('Proporcione la velocidad: '))
+Tiempo = int(input('Proporcione el tiempo: '))
+Distancia = Velocidad * Tiempo
+print(f'La distancia recorrida es: {Distancia} Km/h')
