a85ea15d38ab33398d29eca484269c30fecbc7d1
Erick <erickepc2099@gmail.com>
2022-08-08 20:36:36 -0400

    Actualizar registros en la base de datos

---
+++ PostgreSQL/Update.py
@@ -0,0 +1,17 @@
+import psycopg2 as db
+
+Conexion = db.connect(user='postgres', password='admin20', host='127.0.0.1', port='5432', database='Escuela')
+
+try:
+    with Conexion as CON:
+        with Conexion.cursor() as CURSOR:
+            sentencia = 'UPDATE ESTUDIANTES Set nombre = %s, apellido = %s, email = %s, edad = %s WHERE id_estudiante = %s'
+            valores = ('Enrique', 'PC', 'Actualiza@prueba.com', 24, 1)
+            CURSOR.execute(sentencia, valores)
+            registro = CURSOR.rowcount
+            print(f'Registro Actualizado: {registro}')
+except Exception as ex:
+    print(f'Error: {ex}')
+
+finally:
+    Conexion.close()
