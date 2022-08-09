afb457e199845f3bf6de2cce748f4f943e9032d8
Erick <erickepc2099@gmail.com>
2022-08-08 20:36:49 -0400

    Borrar registros en la base de datos

---
+++ PostgreSQL/Delete.py
@@ -0,0 +1,17 @@
+import psycopg2 as db
+
+Conexion = db.connect(user='postgres', password='admin20', host='127.0.0.1', port='5432', database='Escuela')
+
+try:
+    with Conexion as CON:
+        with Conexion.cursor() as CURSOR:
+            sentencia = 'DELETE FROM Estudiantes where id_estudiantes = %s'
+            valores = (1)
+            CURSOR.execute(sentencia, valores)
+            registro = CURSOR.rowcount
+            print(f'Registro Eliminado: {registro}')
+except Exception as ex:
+    print(f'Error: {ex}')
+
+finally:
+    Conexion.close()
