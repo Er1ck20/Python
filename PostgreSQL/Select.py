0d4adfd37e48e66c696e0527ff411b0600d45832
Erick <erickepc2099@gmail.com>
2022-10-04 18:52:04 -0400

    Select en PostgreSQL

--- PostgreSQL/Select.py
+++ PostgreSQL/Select.py
@@ -3,19 +3,18 @@ import psycopg2 as db
 
 Conexion = db.connect(user='postgres', password='admin20', host='127.0.0.1', port='5432', database='Escuela')
 
-def Mostrar():
-    try:
-        with Conexion as CON:
-            with Conexion.cursor() as CURSOR:
-                sentencia = 'Select * from Estudiantes'
-                CURSOR.execute(sentencia)
-                registro = CURSOR.fetchall()
-                for registros in registro:
-                    print(registros)
-    except Exception as ex:
-        print(f'Error: {ex}')
+# def Mostrar():
+try:
+    with Conexion as CON:
+        with CON.cursor() as CURSOR:
+            sentencia = 'Select * from Estudiantes'
+            CURSOR.execute(sentencia)
+            registro = CURSOR.fetchall()
+            for registros in registro:
+                print(registros)
+except Exception as ex:
+    print(f'Error: {ex}')
 
-    finally:
-        Conexion.close()
+finally:
+    Conexion.close()
 
-print(Mostrar())
