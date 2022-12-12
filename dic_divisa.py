22b51a1dd4cf7cafd31d3a15f262f4c87e31a8be
Er1ck20 <Erickepc2099@gmail.com>
2022-12-11 22:18:57 -0400

    Ejercicio diccionario divisas

--- dic_divisa.py
+++ dic_divisa.py
@@ -1,27 +1,30 @@
 # Escribir un programa que guarda en una variable el diccionario {'Euro': '€', 'Dolar': '$', 'Yen': '¥'}, pregunte al usuario por una divisa y muestre su simbolo, y muestra un mensaje si la divisa no esta en el dic.
 
-dic = {'Euro': '€',
-'Dolar': '$',
-'Yen': '¥'}
+dic_divisa = {'Euro': '€','Dolar': '$','Yen': '¥'}
 
-divisa = input('Que divisa desea ver? ')
+# Ejercicio 1
+x = input('Que divisa desea ver? ')
 
-if divisa == 'Dolar':
-    print(f'{divisa} - {dic["Dolar"]}')
+if x == 'Dolar':
+    print(f'{x} - {dic_divisa["Dolar"]}')
 
-elif divisa == 'Euro':
-    print(f'{divisa} - {dic["Euro"]}')
+elif x == 'Euro':
+    print(f'{x} - {dic_divisa["Euro"]}')
 
-elif divisa == 'Yen':
-    print(f'{divisa} - {dic["Yen"]}')
+elif x == 'Yen':
+    print(f'{x} - {dic_divisa["Yen"]}')
 
 else:
-    print(f'No proporciono ninguna divisa')
+    print(f'No se encontro la divisa')
 
 
+# Ejercicio #2
 # Otra forma de hacerlo.
-
-divisa2 = input('Que divisa desea ver? ')
+y = input('Que divisa desea ver? ')
 # title() - Devolvera la primera palabra de cada oracion en mayuscula.
-divisa2 = divisa2.title()
-print(dic[divisa2])
+y = y.title()
+
+if y in dic_divisa:
+    print(f'{y} - {dic_divisa[y]}')
+else:
+    print(f'No se encontro la divisa')
