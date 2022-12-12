c4d1dfb4966af8cb7604d2ef475755d3cb3ac326
Er1ck20 <Erickepc2099@gmail.com>
2022-12-11 22:05:31 -0400

    Ejercicio - Visualizando divisas

---
+++ dic_divisa.py
@@ -0,0 +1,19 @@
+# Escribir un programa que guarda en una variable el diccionario {'Euro': '€', 'Dolar': '$', 'Yen': '¥'}, pregunte al usuario por una divisa y muestre su simbolo, y muestra un mensaje si la divisa no esta en el dic.
+
+dic = {'Euro': '€',
+'Dolar': '$',
+'Yen': '¥'}
+
+divisa = input('Que divisa desea ver? ')
+
+if divisa == 'Dolar':
+    print(f'{divisa} - {dic["Dolar"]}')
+
+elif divisa == 'Euro':
+    print(f'{divisa} - {dic["Euro"]}')
+
+elif divisa == 'Yen':
+    print(f'{divisa} - {dic["Yen"]}')
+
+else:
+    print(f'No proporciono ninguna divisa')
